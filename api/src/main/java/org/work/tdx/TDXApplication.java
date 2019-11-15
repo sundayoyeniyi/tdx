6db053a192315e8144e563deba24e312e1e82e9f
sundayoyeniyi <sunday.oyeniyi@gmail.com>
2019-11-15 21:02:31 +0000

    feature/apimodule - Adding a blank spring-boot api backend application

---
+++ api/src/main/java/org/work/tdx/TDXApplication.java
@@ -0,0 +1,13 @@
+package org.work.tdx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TDXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TDXApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/org/work/tdx/TDXApplicationTests.java
@@ -0,0 +1,13 @@
+package org.work.tdx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TDXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
