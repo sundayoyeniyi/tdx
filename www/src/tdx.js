db34f91fcb364f031b44ef827f97b8ba4c1c2b12
sundayoyeniyi <sunday.oyeniyi@gmail.com>
2019-11-29 20:32:20 +0000

    update/frontend - Update to starter project
    - removal of create-react-app branding except favicon
    - support for scss post-processor with node-sass
    - Automatic code formatting with git hook using prettier as code formatter
    - Task to bundle analysis

---
+++ www/src/tdx.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./tdx.scss";
+
+function Tdx() {
+  return (
+    <div className="tdx">
+      <header className="tdx-header">TDX - TheDataXplorer</header>
+    </div>
+  );
+}
+
+export default Tdx;
---
+++ www/src/tdx.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Tdx from "./tdx";
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<Tdx />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
